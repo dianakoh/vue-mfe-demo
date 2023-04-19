38fc3d89f75db1b8537178041c7e7c759f2e67f2
gayoung.koh <gayoung.koh@qcells.com>
2023-04-19 23:58:29 +0900

    add host and common-components projects

---
+++ common-components/vue.config.js
@@ -0,0 +1,78 @@
+// const { defineConfig } = require("@vue/cli-service");
+// module.exports = defineConfig({
+//   transpileDependencies: true,
+// });
+
+const path = require('path');
+const { defineConfig } = require('@vue/cli-service');
+const webpack = require('webpack');
+
+const isProduction = process.env.NODE_ENV === 'production';
+const isLocalEnv = process.env.VUE_APP_ENV === 'local';
+const configureWebpackProd = {
+  mode: 'production',
+  devtool: 'source-map',
+};
+const configureWebpackDev = {
+  mode: 'development',
+  devtool: 'source-map',
+};
+const configureWebpack = isProduction ? configureWebpackProd : configureWebpackDev;
+
+console.log(
+  '[ENV Info]',
+  'process.env.NODE_ENV: ',
+  process.env.NODE_ENV,
+  ', process.env.VUE_APP_ENV: ',
+  process.env.VUE_APP_ENV
+);
+
+module.exports = defineConfig({
+  assetsDir: isLocalEnv ? 'resource/' : '../resources/',
+  pages: {
+    index: {
+      entry: './src/index.ts',
+    },
+  },
+  publicPath: 'auto',
+  devServer: {
+    port: 8081,
+    historyApiFallback: true,
+  },
+  configureWebpack: {
+    optimization: {
+      splitChunks: {
+        cacheGroups: {
+          defaultVendors: {
+            name: 'chunk-vendors',
+            test: /[\\/]node_modules[\\/]/,
+            priority: -10,
+            chunks: 'async',
+            reuseExistingChunk: true,
+          },
+          common: {
+            name: 'chunk-common',
+            minChunks: 2,
+            priority: -20,
+            chunks: 'async',
+            reuseExistingChunk: true,
+          },
+        },
+      },
+    },
+    plugins: [
+      new webpack.container.ModuleFederationPlugin({
+        name: 'vueRemote',
+        filename: 'remoteEntry.js',
+        remotes: {},
+        exposes: {},
+        shared: {
+          vue: {
+            singleton: true,
+          },
+        },
+      }),
+    ],
+  },
+  transpileDependencies: true,
+});
---
+++ host/vue.config.js
@@ -0,0 +1,58 @@
+// const { defineConfig } = require("@vue/cli-service");
+// module.exports = defineConfig({
+//   transpileDependencies: true,
+// });
+/* eslint-disable @typescript-eslint/no-var-requires */
+const path = require('path');
+const { defineConfig } = require('@vue/cli-service');
+const webpack = require('webpack');
+
+const isProduction = process.env.NODE_ENV === 'production';
+const isLocalEnv = process.env.VUE_APP_ENV === 'local';
+const configureWebpackProd = {
+  mode: 'production',
+  devtool: 'source-map',
+};
+const configureWebpackDev = {
+  mode: 'development',
+  devtool: 'source-map',
+};
+const configureWebpack = isProduction ? configureWebpackProd : configureWebpackDev;
+
+console.log(
+  '[ENV Info]',
+  'process.env.NODE_ENV: ',
+  process.env.NODE_ENV,
+  ', process.env.VUE_APP_ENV: ',
+  process.env.VUE_APP_ENV
+);
+
+module.exports = defineConfig({
+  assetsDir: isLocalEnv ? 'resource/' : '../resources/',
+  pages: {
+    index: {
+      entry: './src/index.ts',
+    },
+  },
+  publicPath: 'auto',
+  devServer: {
+    port: 8080,
+    historyApiFallback: true,
+  },
+  configureWebpack: {
+    plugins: [
+      new webpack.container.ModuleFederationPlugin({
+        name: 'host',
+        filename: 'remoteEntry.js',
+        remotes: {},
+        exposes: {},
+        shared: {
+          vue: {
+            singleton: true,
+          },
+        },
+      }),
+    ],
+  },
+  transpileDependencies: true,
+});
